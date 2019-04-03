778e7f34c036edf76a276c17096261f1e7ecd415
2CS\quannguyen <quan.nguyen@2clicksolutions.com>
2019-04-03 16:13:59 +0700

    Finish chapter 3

--- c3.mjs
+++ c3.mjs
@@ -1,4 +1,5 @@
 import * as fp from "./fp";
+import { curryProps } from "./fp.mjs";
 
 // One in All
 var onInAll = ["1", "2", "3"].map(fp.unary(parseInt));
@@ -8,3 +9,77 @@ console.log(`One in All: ${onInAll}`);
 var words = "   Now is the time for all...  ".split(/\s|\b/);
 var oneOnOne = words.filter(fp.identity);
 console.log(`One on One: ${oneOnOne}`);
+
+// Partial Right
+function fooPR(x, y, z, ...rest) {
+  console.log(x, y, z, rest);
+}
+
+var f = fp.partialRight(fooPR, 'z:last');
+f(1, 2);
+f(1);
+f(1, 2, 3);
+f(1, 2, 3, 4);
+
+
+// Curry function.
+function add(x, y) {
+  return x + y;
+}
+
+var adder = fp.curry(add);
+var curried = [1, 2, 3, 4, 5].map(adder(3));
+console.log(curried);
+
+function sum(...nums) {
+  var total = 0;
+  for (let num of nums) {
+    total += num;
+  }
+
+  return total;
+}
+
+var curriedSum = fp.curry(sum, 5);
+console.log(sum(1, 2, 3, 4, 5));
+console.log(curriedSum(1)(2)(3)(4)(5));
+
+// Uncurry.
+var uncurriedSum = fp.uncurry(curriedSum);
+
+console.log(uncurriedSum(1, 2, 3, 4, 5));
+console.log(uncurriedSum(1, 2, 3)(4)(5));
+
+// Order matters.
+
+function foo({x, y, z} = {}) {
+  console.log(`x:${x} y:${y} z:${z}`);
+}
+
+var f1 = fp.curryProps(foo, 3);
+var f2 = fp.partialProps(foo, { y: 2 });
+
+f1({ y: 2 })({ x: 1 })({ z: 3 });
+f2({ z: 3, x: 1 });
+
+// Point free.
+
+function output(msg) {
+  console.log(msg);
+}
+
+function isShortEnough(str) {
+  return str.length <= 5;
+}
+
+var isLongEnough = fp.not(isShortEnough);
+var printIf = fp.uncurry(fp.partialRight(fp.when, output));
+
+var msg1 = 'Hello';
+var msg2 = msg1 + ' World';
+
+printIf(isShortEnough, msg1);
+printIf(isShortEnough, msg2);
+
+printIf(isLongEnough, msg1);
+printIf(isLongEnough, msg2);
--- fp.mjs
+++ fp.mjs
@@ -32,4 +32,107 @@ function partial(fn, ...presetArgs) {
   };
 }
 
-export { unary, identity, constant, spreadArgs, gatherArgs, partial };
+function reverseArgs(fn) {
+  return function argsReversed(...args) {
+    return fn(...args.reverse());
+  };
+}
+
+function partialRight(fn, ...presetArgs) {
+  return function partiallyApplied(...laterArgs) {
+    return fn(...laterArgs, ...presetArgs);
+  };
+}
+
+function curry(fn, arity = fn.length) {
+  return (function nextCurried(prevArgs) {
+    return function curried(nextArg) {
+      var args = [...prevArgs, nextArg];
+
+      if (args.length >= arity) {
+        return fn(...args);
+      } else {
+        return nextCurried(args);
+      }
+    };
+  })([]);
+}
+
+function looseCurry(fn, arity = fn.length) {
+  return (function nextCurried(prevArgs) {
+    return function looseCurried(...nextArgs) {
+      var args = [...prevArgs, ...nextArgs];
+
+      if (args.length >= arity) {
+        return fn(...args);
+      } else {
+        return nextCurried(args);
+      }
+    };
+  })([]);
+}
+
+function uncurry(fn) {
+  return function uncurried(...args) {
+    var ret = fn;
+
+    for (let arg of args) {
+      ret = ret(arg);
+    }
+
+    return ret;
+  };
+}
+
+function partialProps(fn, presetArgsObj) {
+  return function partiallyApplied(laterArgsObj) {
+    return fn({...presetArgsObj, ...laterArgsObj});
+  };
+}
+
+function curryProps(fn, arity = 1) {
+  return (function nextCurried(prevArgsObj) {
+    return function curried(nextArgObj = {}) {
+      var [firstKey] = Object.keys(nextArgObj);
+      var allArgsObj = { ...prevArgsObj, [firstKey]: nextArgObj[firstKey] };
+
+      if (Object.keys(allArgsObj).length >= arity) {
+        return fn(allArgsObj);
+      } else {
+        return nextCurried(allArgsObj);
+      }
+    };
+  })({});
+}
+
+function not(predicate) {
+  return function negated(...args) {
+    return !predicate(...args);
+  };
+}
+
+function when(predicate, fn) {
+  return function conditional(...args) {
+    if (predicate(...args)) {
+      return fn(...args);
+    }
+  };
+}
+
+export {
+  unary,
+  identity,
+  constant,
+  spreadArgs,
+  gatherArgs,
+  partial,
+  reverseArgs,
+  partialRight,
+  curry,
+  looseCurry,
+  uncurry,
+  partialProps,
+  curryProps,
+  not,
+  when,
+};
